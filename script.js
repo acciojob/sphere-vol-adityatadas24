e1c36a9814d5522e48312ffd2dbaa1ffd61ed19f
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-30 11:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 function volume_sphere() {
-    // Get the value of the Radius field
+  
     const radiusInput = document.getElementById("Radius");
     const radius = parseFloat(radiusInput.value);
 
-    // Check if the input is a valid number
+
     if (!isNaN(radius) && radius >= 0) {
-        // Calculate the volume of the sphere (assuming PI is 3.14159265359)
+       
         const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
-        // Update the Volume field with the calculated volume
+       
         const volumeField = document.getElementById("Volume");
-        volumeField.value = volume.toFixed(2); // Display the volume with 2 decimal places
+        volumeField.value = volume.toFixed(2); 
     } else {
-        // If the input is not a valid positive number, display an error message
-        alert("Please enter a valid positive number for the radius.");
+     
+      document.getElementById("volumn").value = "NaN";
     }
+	e.preventDefault();
 }
 
-
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+
